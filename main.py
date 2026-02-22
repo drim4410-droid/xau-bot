4f45560066d17ee51114119c2be5652aee302619
drim4410-droid <drim4410@gmail.com>
2026-02-22 16:50:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import requests
 import time
 import os
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+def run_web():
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
@@ -18,20 +29,23 @@ def get_gold_price():
     response = requests.get(url).json()
     return float(response[0]["price"])
 
-last_price = None
+def bot_loop():
+    last_price = None
+    while True:
+        try:
+            price = get_gold_price()
 
-while True:
-    try:
-        price = get_gold_price()
+            if last_price:
+                if price > last_price + 3:
+                    send_message(f"📈 BUY XAU/USD\nPrice: {price}")
+                elif price < last_price - 3:
+                    send_message(f"📉 SELL XAU/USD\nPrice: {price}")
 
-        if last_price:
-            if price > last_price + 3:
-                send_message(f"📈 BUY XAU/USD\nPrice: {price}")
-            elif price < last_price - 3:
-                send_message(f"📉 SELL XAU/USD\nPrice: {price}")
+            last_price = price
+            time.sleep(300)
+        except:
+            time.sleep(60)
 
-        last_price = price
-        time.sleep(300)  # 5 минут
-
-    except:
-        time.sleep(60)
+if __name__ == "__main__":
+    Thread(target=run_web).start()
+    bot_loop()
